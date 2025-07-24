b2cafc4e20c693bcfe3a2041eadfe3aef377ec02
Shubham Dogra <shubham@192.168.1.3>
2025-07-24 13:29:51 +0530

    fixed one code

--- dynamicProgamming/maximumSumNonAdjacents.java
+++ dynamicProgamming/maximumSumNonAdjacents.java
@@ -2,36 +2,39 @@
 Maximum sum such that no two elements are adjacent
 */
 import java.util.Scanner;
+
 class maximumSumNonAdjacents {
 
-	/*with O(N) space
+    /*with O(N) space
 	1: make an dp array of size n+1. where n is length of nums array.
 	2: dp[0] will denote max profit with 0 number of elements. hence it is 0.
 	3: dp[1] will be max profit with 1 element. which is num[0].
 	4: now for every next element i.e. dp[2] onwards we have a choice to
-	   either include the alternative element or exclude it so that we can 
+	   either include the alternative element or exclude it so that we can
 	   make more profit.
 	*/
-	public static int solve(int[] nums) {
-		int[] dp = new int[nums.length + 1];
-		dp[0] = 0;
-		dp[1] = nums[0];
-		for (int i = 2; i < dp.length; i++) {
-			dp[i] = Math.max(dp[i - 2] + nums[i - 1] , dp[i - 1]);
-		}
+    public static int solve(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            int take = nums[i] + (i >= 2 ? dp[i - 2] : 0);
+            int notTake = dp[i - 1];
+            dp[i] = Math.max(take, notTake);
+        }
+        return dp[n - 1];
+    }
 
-		return dp[nums.length];
-	}
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = scn.nextInt();
-		}
-		scn.close();
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
 
-		/*
+        /*
 		maintain two sums incl and excl where incl = Max sum including the
 		previous element and excl = Max sum excluding the previous
 		element. Max sum excluding the current element will be max
@@ -40,16 +43,16 @@ class maximumSumNonAdjacents {
 		elements cannot be adjacent).
 		*/
 
-		int inc = arr[0]; //0th number included
-		int exc = 0;     //0th number not included
-		for (int i = 1; i < arr.length; i++) {
-			int nInc = exc + arr[i];
-			int nExc = Math.max(inc , exc);
+        int inc = arr[0]; //0th number included
+        int exc = 0; //0th number not included
+        for (int i = 1; i < arr.length; i++) {
+            int nInc = exc + arr[i];
+            int nExc = Math.max(inc, exc);
 
-			inc = nInc;
-			exc = nExc;
-		}
+            inc = nInc;
+            exc = nExc;
+        }
 
-		System.out.println(Math.max(inc , exc));
-	}
+        System.out.println(Math.max(inc, exc));
+    }
 }
